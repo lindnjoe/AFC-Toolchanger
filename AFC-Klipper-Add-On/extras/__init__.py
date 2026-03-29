19825cab5a727fae91a2d6cdd99a7e8ec06958c7
J0eB0l <32586000+lindnjoe@users.noreply.github.com>
2026-03-29 13:55:35 -0400

    Delete AFC-Klipper-Add-On directory

--- AFC-Klipper-Add-On/extras/__init__.py
+++
@@ -1,3 +0,0 @@
-# This file makes the extras directory a Python package,
-# enabling cross-module imports (from extras.AFC_X import Y)
-# in the test environment. Klipper itself does not rely on this file.
